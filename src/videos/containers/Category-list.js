361799d3bb414a808a8dc332335be1ca5c6e3e02
edwin <spark.com.co@gmail.com>
2020-02-26 17:34:50 -0500

    Separadores horizontales

--- src/videos/containers/Category-list.js
+++ src/videos/containers/Category-list.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {FlatList} from 'react-native';
+import Layout from '../components/Category-list-layout';
 import Empty from '../components/Empty';
-import Separator from '../components/Separator';
+import Separator from '../components/Horizontal-separator';
 import Suggestion from '../components/Suggestion';
 
 const CategoryList = ({categories}) => {
@@ -14,14 +15,16 @@ const CategoryList = ({categories}) => {
   const key = item => item.id.toString();
 
   return (
-    <FlatList
-      horizontal
-      keyExtractor={key}
-      data={categories}
-      ListEmptyComponent={renderEmpty}
-      ItemSeparatorComponent={renderSeparator}
-      renderItem={renderSuggestion}
-    />
+    <Layout title="Categorías">
+      <FlatList
+        horizontal
+        keyExtractor={key}
+        data={categories}
+        ListEmptyComponent={renderEmpty}
+        ItemSeparatorComponent={renderSeparator}
+        renderItem={renderSuggestion}
+      />
+    </Layout>
   );
 };
 
