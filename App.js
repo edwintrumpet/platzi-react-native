038e4b6d51ea980d7b1b738962ffd81761a8dcff
edwin <spark.com.co@gmail.com>
2020-02-26 18:34:37 -0500

    Instalando plugins de la comunidad: Video

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/Suggestion-list';
 import Api from './src/utils/api';
 import CategoryList from './src/videos/containers/Category-list';
+import Video from 'react-native-video';
+import {View, StyleSheet} from 'react-native';
 
 const App = () => {
   const [list, setList] = useState([]);
@@ -25,10 +27,33 @@ const App = () => {
   return (
     <Home>
       <Header />
+      <View style={styles.container}>
+        <Video
+          source={{
+            uri: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+          }}
+          style={styles.video}
+          resizeMode="contain"
+        />
+      </View>
       <CategoryList categories={categories} />
       <SuggestionList list={list} />
     </Home>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 100,
+  },
+  video: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+  },
+});
 
 export default App;
