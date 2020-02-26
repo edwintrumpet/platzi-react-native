024b6acba3e069c727510331808d3628c005ccbe
edwin <spark.com.co@gmail.com>
2020-02-25 23:03:09 -0500

    Haciendo peticiones a un servidor: fetch

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/Suggestion-list';
+import Api from './src/utils/api';
 
-const App = () => (
-  <Home>
-    <Header />
-    <SuggestionList />
-  </Home>
-);
+const App = () => {
+  useEffect(() => {
+    (async () => {
+      const movies = await Api.getSugerencias(10);
+      console.log(movies);
+    })();
+  }, []);
+
+  return (
+    <Home>
+      <Header />
+      <SuggestionList />
+    </Home>
+  );
+};
 
 export default App;
