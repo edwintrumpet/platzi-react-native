375a960c99c5022714e162ea3ac9368e9f6d34c2
edwin <spark.com.co@gmail.com>
2020-02-26 19:35:53 -0500

    Controlando el buffer del video

--- src/player/components/Layout.js
+++ src/player/components/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-const Layout = ({video}) => (
+const Layout = ({video, loader, loading}) => (
   <View style={styles.container}>
     <View style={styles.video}>{video}</View>
+    <View style={styles.overlay}>{loading && loader}</View>
   </View>
 );
 
@@ -18,6 +19,15 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     backgroundColor: 'black',
+  },
+  overlay: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
--- src/player/containers/Player.js
+++ src/player/containers/Player.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, ActivityIndicator} from 'react-native';
 import Video from 'react-native-video';
 import Layout from '../components/Layout';
 
-const Player = () => (
-  <Layout
-    video={
-      <Video
-        source={{
-          uri: 'http://media.w3.org/2010/05/bunny/movie.mp4',
-        }}
-        style={styles.video}
-        resizeMode="contain"
-      />
-    }
-  />
-);
+const Player = () => {
+  const [loading, setLoading] = useState(true);
+
+  const onBuffer = ({isBuffering}) => {
+    setLoading(isBuffering);
+  };
+
+  const onLoad = () => {
+    setLoading(false);
+  };
+
+  return (
+    <Layout
+      loading={loading}
+      video={
+        <Video
+          source={{
+            uri: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+          }}
+          style={styles.video}
+          resizeMode="contain"
+          onBuffer={onBuffer}
+          onLoad={onLoad}
+        />
+      }
+      loader={<ActivityIndicator color="white" />}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
   video: {
