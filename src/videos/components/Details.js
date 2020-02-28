347e101b70e4a3c8e8ecffb65c146564fa8435e5
edwin <spark.com.co@gmail.com>
2020-02-27 21:45:43 -0500

    Scroll view y WebView

--- src/videos/components/Details.js
+++ src/videos/components/Details.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-const Details = ({title, description_full, medium_cover_image}) => (
-  <View>
-    <View style={styles.top}>
-      <Text>{title}</Text>
-    </View>
-    <View style={styles.bottom}>
-      <View style={styles.details}>
-        <Image source={{uri: medium_cover_image}} style={styles.cover} />
-        <Text style={styles.description}>{description_full}</Text>
+const Details = ({
+  title,
+  description_full,
+  medium_cover_image,
+  yt_trailer_code,
+}) => {
+  const makeHTML = id => `
+    <style>
+        .video {
+            position: relative;
+            padding-bottom: 56.25%;
+        }
+        iframe {
+            position: absolute;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+    <div class="video">
+        <iframe
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/${id}" 
+            frameborder="0" 
+            allow="accelerometer; 
+            autoplay; 
+            encrypted-media; 
+            gyroscope; 
+            picture-in-picture" 
+            allowfullscreen
+        ></iframe>
+    </div>`;
+
+  return (
+    <ScrollView>
+      <View style={styles.top}>
+        <Text>{title}</Text>
       </View>
-    </View>
-  </View>
-);
+      <View style={styles.bottom}>
+        <View style={styles.details}>
+          <Image source={{uri: medium_cover_image}} style={styles.cover} />
+          <Text style={styles.description}>{description_full}</Text>
+        </View>
+        <View style={styles.trailer}>
+          <WebView source={{html: makeHTML(yt_trailer_code)}} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {},
   trailer: {
     height: 200,
+    marginBottom: 10,
   },
   details: {
     flexDirection: 'row',
